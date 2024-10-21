cbf51f8865bd919601c65871ba2907ccdeded120
ssaakaash <ssaakaash@icloud.com>
2024-10-21 22:16:06 +0530

    Initial Commit

---
+++ sandwiches/sandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandwichesApp.swift
+//  sandwiches
+//
+//  Created by Aakaash SS on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct sandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
